0540ff67a832626cf474cc6213f61953f4e3c9ed
lilblackboot <131806378+lilblackboot@users.noreply.github.cogit config --global user.email 131806378+lilblackboot@users.noreply.github.com>
2025-07-17 12:53:24 +0530

    sections flow change

--- MyApp/Navigation.tsx
+++ MyApp/Navigation.tsx
@@ -6,6 +6,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './page/LoginScreen';
 import ProfileData from './page/ProfileData';
 import HomeScreen from './page/HomeScreen';
+import Sections from './page/Sections'
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +16,7 @@ export default function Navigation() {
       <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="ProfileData" component={ProfileData} />
+        <Stack.Screen name="Sections" component={Sections} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
